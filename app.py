fce130697ceb39a21944b071c5affc208206fadb
John Prabhakaran <johnprabhakaran@deanta.net>
2024-12-17 17:31:09 +0530

    Temp

--- app.py
+++ app.py
@@ -14,6 +14,19 @@ def get_random_image(folder):
             return os.path.join(folder, random.choice(image_files))
     return None
 
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 # Create an image placeholder
 image_placeholder = st.empty()
 
