c63af543deb92367e1cfd9d283aef488c098aeaa
John Prabhakaran <johnprabhakaran@deanta.net>
2024-12-17 12:13:21 +0530

    FUlly updated

--- app.py
+++ app.py
@@ -7,28 +7,23 @@ import time
 IMAGE_DIR = "./images/"
 WISH_IMAGE_DIR = "./images/wishes/"
 
+def get_random_image(folder):
+    if os.path.exists(folder):
+        image_files = [f for f in os.listdir(folder)]
+        if image_files:
+            return os.path.join(folder, random.choice(image_files))
+    return None
 
-# Check if the directory exists and contains images
-if os.path.exists(IMAGE_DIR):
-    image_files = [f for f in os.listdir(IMAGE_DIR)]
+# Create an image placeholder
+image_placeholder = st.empty()
 
-    if image_files:
-        # Select a random image when the page loads
-        random_image = random.choice(image_files)
-        image_path = os.path.join(IMAGE_DIR, random_image)
-        st.image(image_path, use_container_width =True)
+# Display a random image initially
+random_image_path = get_random_image(IMAGE_DIR)
+if random_image_path:
+    image_placeholder.image(random_image_path, use_container_width =True)
 
-        time.sleep(5)
+time.sleep(5)
 
-        wish_image_files = [f for f in os.listdir(WISH_IMAGE_DIR)]
-        if wish_image_files:
-            random_image = random.choice(wish_image_files)
-            image_path = os.path.join(IMAGE_DIR, random_image)
-            st.image(image_path, use_container_width =True)
-
-
-
-    else:
-        st.warning("No image files found in the directory.")
-else:
-    st.error(f"The directory '{IMAGE_DIR}' does not exist.")
+christmas_image_path = get_random_image(WISH_IMAGE_DIR)
+if christmas_image_path:
+    image_placeholder.image(christmas_image_path, use_container_width =True)
