554a35527b197f147dc363d0ebc6b6d26ab57d3a
John Prabhakaran <johnprabhakaran@deanta.net>
2024-12-17 17:43:58 +0530

    Final work

--- app.py
+++ app.py
@@ -17,7 +17,8 @@ def get_random_image(folder):
 st.markdown(
     """
     <style>
-    .css-1jc7ptx, .e1ewe7hr3, .stAppHeader, .viewerBadge_container__1QSob,
+    .css-1jc7ptx, .e1ewe7hr3, .stAppHeader, ._profileContainer_gzau3_53, 
+    .viewerBadge_container__1QSob,
     .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
     .viewerBadge_text__1JaDK {
         display: none;
